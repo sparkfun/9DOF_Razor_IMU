c96c3da9066245c57e3bb0aae847b556a2ec4227
Jim Lindblom <jim@sparkfun.com>
2016-11-04 12:48:58 -0600

    Cleanup bootloader directory.

--- Firmware/Bootloader/build_all_bootloaders.sh
+++ Firmware/Bootloader/build_all_bootloaders.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+BOARD_ID=sparkfun_9dof NAME=SparkFun_9DoF_Razor make clean all
+
 BOARD_ID=arduino_zero NAME=samd21_sam_ba make clean all
 
 BOARD_ID=genuino_zero NAME=samd21_sam_ba_genuino make clean all
